8034b19bb55856c0d18e58af7835b110048d05a0
Shriom Chourasiya <shriom.chourasiya@systematixindia.com>
2019-10-17 14:59:55 +0530

    chnages for calendar

--- App/Screens/CreateToDo/CreateToDo.js
+++ App/Screens/CreateToDo/CreateToDo.js
@@ -4,9 +4,13 @@ import {images} from 'Components/ImageComponent'
 import LocalStorage from '../../Libs/LocalStorage'
 import DateTimePicker from "react-native-modal-datetime-picker";
 import Moment from 'moment';
+import RNCalendarEvents from 'react-native-calendar-events';
 const {width, height} = Dimensions.get('window')
 
 export default class createToDo extends Component{
+    static navigationOptions = {
+        title: 'Add TO DO',
+    };
     constructor(props){
         super(props);
         this.state = {
@@ -17,67 +21,129 @@ export default class createToDo extends Component{
             user_data: [],
             selectedDate: '',
             isDateTimePickerVisible: false,
+            isStartDateTimePickerVisible: false,
+            isEndDateTimePickerVisible: false,
             selected_date: Moment(new Date()).format('DD-MM-YYYY'),
-            todo_date: ''
+            todo_date: '',
+            todo_start_time: '',
+            todo_end_time: '',
+            title:'',
+            description:''
+
         }
         focusListener= null
     }
 
-    componentDidMount(){
-        this.focusListener = this.props.navigation.addListener('willFocus',() => {
-            LocalStorage.getStore((item)=>{
-                this.setState({mobile: '', password: ''})
-                if(item){
-                    this.setState({user_data: item})
-                }
-            })
-        })
-    }
+    
 
     showDateTimePicker = () => {
         this.setState({ isDateTimePickerVisible: true });
+    };
+    showStartDateTimePicker = () => {
+        this.setState({ isStartDateTimePickerVisible: true });
+    };
+    showEndDateTimePicker = () => {
+            this.setState({ isEndDateTimePickerVisible: true });
     };
     
     hideDateTimePicker = () => {
         this.setState({ isDateTimePickerVisible: false });
     };
+
+    hideStartDateTimePicker = () => {
+        this.setState({ isStartDateTimePickerVisible: false });
+    };
+    hideEndDateTimePicker = (mode) => {
+        this.setState({ isEndDateTimePickerVisible: false });
+    };
+
+
     
     handleDatePicked = (date) => {
         console.log("A date has been picked: ", Moment(date).format('DD-MM-YYYY'));
-        this.setState({selected_date: date, todo_date: Moment(date, 'DD-MM-YYYY')})
+        this.setState({selected_date: Moment(date).format('YYYY-MM-DD'), todo_date: Moment(date, 'DD-MM-YYYY')})
         this.hideDateTimePicker();
     };
+    handleStartDatePicked = (date) => {
+        console.log("A date has been picked: ", Moment(date).format('hh:mm A'));
+        this.setState({selected_start_time: Moment(date).format('HH:mm:ss'), todo_start_time: Moment(date, 'hh:mm A')})
+        this.hideStartDateTimePicker();
+    };
+    handleEndDatePicked = (date) => {
+        console.log("A date has been picked: ", Moment(date).format('HH:mm:ss'));
+        this.setState({selected_end_time: Moment(date).format('HH:mm:ss'), todo_end_time: Moment(date, 'hh:mm A')})
+        this.hideEndDateTimePicker();
+    };
 
-    loginPress = () =>{
-        const {mobile, password, user_data} = this.state
-        if (!mobile) {
-            this.setState({ isMobileEmpty: true})
-        } else if (mobile.length < 10) {
-            this.setState({ isMobileEmpty: true})
-        }else if(!password){
-            this.setState({ isPwdEmpty: true})
+    AddPress = () =>{
+      
+        const {todo_date,todo_start_time,todo_end_time,title,description} = this.state;
+        if (title.length < 1) {
+            alert("Please enter title")
+        } else if (description.length < 1) {
+            alert("Please enter description")
+        }else if (todo_date.length < 1) {
+            alert("Please select todo date")
+        }else if (todo_start_time.length < 1) {
+            alert("Please select todo start time")
+        }else if (todo_end_time.length < 1) {
+            alert("Please select todo end time")
+        }else if (Moment(todo_start_time,"hh:mm A").format('x') > Moment(todo_end_time,"hh:mm A").format('x')) {
+            alert("Please select todo end time is greter then todo  start time")
         }else{
-            if(user_data && user_data.length>0){
-                var found = false
-                user_data.map((item)=>{
-                    if(item.mobile_number == mobile){
-                        found = true
-                        this.setState({mobile: '', password: ''})
-                    }
-                })
-                if(!found){
-                    alert('Invalid username or password')
-                }
-            }else{
-                alert('Invalid username or password')
-            }
+            var startDate =  Moment.utc(this.state.selected_date +" "+ this.state.selected_start_time,"YYYY-MM-DD HH:mm:ss").format("YYYY-MM-DDTHH:mm:ss.SSS[Z]")
+            console.log("startDatestartDatestartDatestartDate",startDate)
+            var endDate = Moment.utc(this.state.selected_date +" "+ this.state.selected_end_time,"YYYY-MM-DD HH:mm:ss").format("YYYY-MM-DDTHH:mm:ss.SSS[Z]")
+            RNCalendarEvents.saveEvent(title, {
+                calendarId: this.state.calendarID,
+                startDate: startDate,
+                endDate: endDate,
+                notes: description
+              }).then((value) => {
+                this.props.navigation.goBack()
+                this.props.navigation.state.params.returnData();
+              });
         }
     }
 
+    handleChange=(text,value)=>{
+        this.setState({[text]:value})
+    }
+    
+    componentWillMount(){
+        calendar = { 
+            title:"TODO",
+            color:"orange",
+            entityType:"event",
+        }
+        AsyncStorage.getItem("calendarID").then((value) => {
+            if(value == null){
+                RNCalendarEvents.saveCalendar(calendar).then((value) => {
+                    AsyncStorage.setItem("calendarID",value)
+                    this.setState({calendarID:value})
+                })
+            }else{
+                this.setState({calendarID:value})
+            }
+        })
+    }
+
     render(){
-        const {todo_date} = this.state
+        const {todo_date,todo_start_time,todo_end_time,title,description} = this.state
         return(
             <View style={styles.container}>
+                <TextInput 
+                    style={[styles.date_filter,{padding:5}]} 
+                    placeholder="Enter title"
+                    value={title}
+                    onChangeText={(text)=>{this.handleChange("title",text)}}
+                    />
+                <TextInput 
+                    style={[styles.date_filter,{padding:5}]} 
+                    placeholder="Enter description"
+                    value={description}
+                    onChangeText={(text)=>{this.handleChange("description",text)}}
+                    ></TextInput>
                 <TouchableOpacity onPress={this.showDateTimePicker}>
                     <View style={styles.date_filter}>
                         <Text>{todo_date ? Moment(new Date(todo_date)).format('DD-MM-YYYY') : 'Enter todo date'}</Text>
@@ -88,6 +154,37 @@ export default class createToDo extends Component{
                         onCancel={this.hideDateTimePicker}
                         minimumDate={new Date()}
                     />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.showStartDateTimePicker}>
+                    <View style={styles.date_filter}>
+                        <Text>{todo_start_time ? Moment(new Date(todo_start_time)).format('hh:mm A') : 'Enter todo start time'}</Text>
+                    </View>
+                    <DateTimePicker
+                        isVisible={this.state.isStartDateTimePickerVisible}
+                        onConfirm={this.handleStartDatePicked}
+                        onCancel={this.hideStartDateTimePicker}
+                        is24Hour ={false}
+                        mode={"time"}
+                        minimumDate={new Date()}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.showEndDateTimePicker}>
+                    <View style={styles.date_filter}>
+                        <Text>{todo_end_time ? Moment(new Date(todo_end_time)).format('hh:mm A') : 'Enter todo end time'}</Text>
+                    </View>
+                    <DateTimePicker
+                        isVisible={this.state.isEndDateTimePickerVisible}
+                        onConfirm={this.handleEndDatePicked}
+                        onCancel={this.hideEndDateTimePicker}
+                        is24Hour ={false}
+                        mode={"time"}
+                        minimumDate={new Date()}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity  onPress={()=>this.AddPress()}>
+                    <View style={[styles.date_filter,{backgroundColor:"green"}]}>
+                        <Text style={{textAlign:'center',color:"white",fontSize:20,fontWeight:'800'}}>Add</Text>
+                    </View>
                 </TouchableOpacity>
             </View>
         )
@@ -104,11 +201,13 @@ const styles = StyleSheet.create({
     date_filter:{
         width: '100%',
         height: 50,
+        marginVertical:5,
+        padding:5,
         borderWidth: 1,
         borderRadius: 10,
         borderColor: '#ccc',
         justifyContent: 'center',
-        alignItems: 'center'
+        // alignItems: 'center'
     }
     
 })
--- App/Screens/ToDoList/ToDoList.js
+++ App/Screens/ToDoList/ToDoList.js
@@ -1,10 +1,14 @@
 import React, {useState, Component} from 'react'
-import {StyleSheet, View, Text, Dimensions, ScrollView, AsyncStorage, ImageBackground, Image, TextInput, TouchableOpacity} from 'react-native'
+import {StyleSheet, View, Text, Dimensions, FlatList, AsyncStorage, ImageBackground, Image, TextInput, TouchableOpacity} from 'react-native'
 import {images} from 'Components/ImageComponent'
 import LocalStorage from '../../Libs/LocalStorage'
 const {width, height} = Dimensions.get('window')
-
+import RNCalendarEvents from 'react-native-calendar-events';
+import Moment from "moment"
 export default class ToDoList extends Component{
+    static navigationOptions = {
+        title: 'TO DO List',
+    };
     constructor(props){
         super(props);
         this.state = {
@@ -12,26 +16,73 @@ export default class ToDoList extends Component{
             mobile: '',
             isPwdEmpty: false,
             password: '',
-            user_data: []
+            user_data: [],
+            eventList:[]
         }
         focusListener= null
     }
 
-    componentDidMount(){
-        this.focusListener = this.props.navigation.addListener('willFocus',() => {
-            LocalStorage.getStore((item)=>{
-                this.setState({mobile: '', password: ''})
-                if(item){
-                    this.setState({user_data: item})
-                }
-            })
+    componentWillMount = async()=>{
+        var authorize =  await  RNCalendarEvents.authorizeEventStore();
+        console.log("authorize",authorize)
+        calendar = { 
+            title:"TODO",
+            color:"orange",
+            entityType:"event",
+            // name:"TODO",
+            // ownerAccount:"test",
+            // source:{name:"test",type:"test"}
+        }
+        AsyncStorage.getItem("calendarID").then((value) => {
+            if(value == null){
+                RNCalendarEvents.saveCalendar(calendar).then((value) => {
+                    AsyncStorage.setItem("calendarID",value)
+                    this.setState({calendarID:value})
+                    this.findEvent(value)
+                })
+            }else{
+                this.setState({calendarID:value})
+                this.findEvent(value)
+            }
         })
     }
 
+    findEvent(calendarid){
+        console.log(calendarid)
+        var startDate =  Moment.utc(new Date()).format("YYYY-MM-DDTHH:mm:ss.SSS[Z]")
+        console.log("startDate",startDate)
+        var endDate =  Moment(Moment.utc(new Date()).add("year",1)).format("YYYY-MM-DDTHH:mm:ss.SSS[Z]");
+        console.log("endDate",endDate)
+        RNCalendarEvents.fetchAllEvents(startDate, endDate, [calendarid]).then((value) => {
+            this.setState({eventList:value})
+            console.log("eventListeventList",value)
+        })
+    }
+
+    returnData(){
+        this.findEvent(this.state.calendarID)
+    }
+    renderItem = ({ item, index }) => {
+        return (
+            <View style={{padding:9,backgroundColor:'rgba(0,0,0,0.2)',borderColor:'black',borderRadius:10,borderWidth:2}}>
+                <Text style={{fontSize:22,color:'black'}}><Text style={{fontSize:22,color:'black',fontWeight:"700"}}>Title: </Text>{item.title}</Text>
+                <Text style={{fontSize:18,color:'black'}}><Text style={{fontSize:22,color:'black',fontWeight:"700"}}>Description:</Text> {item.notes}</Text>
+                <Text style={{fontSize:18,color:'black'}}><Text style={{fontSize:22,color:'black',fontWeight:"700"}}>Date and time:</Text> {Moment(item.startDate).format("MM-DD-YYYY hh:mm A")}</Text>
+            </View>
+        )
+    };
     render(){
         return(
             <View style={styles.container}>
-                <TouchableOpacity style={styles.plus_btn} onPress={() => this.props.navigation.navigate('CreateToDo')}>
+                <FlatList
+                    style={{ backgroundColor: '#f0f1f1',flex:1 ,padding:10}}
+                    contentContainerStyle={{ backgroundColor: 'transparent' }}
+                    data={this.state.eventList}
+                    renderItem={this.renderItem}
+                    extraData={this.state}
+                    keyExtractor={item => item.id}
+                />
+                <TouchableOpacity style={styles.plus_btn} onPress={() => this.props.navigation.navigate('CreateToDo',{returnData:this.returnData.bind(this)})}>
                     <Text style={styles.plus_icon}>+</Text>
                 </TouchableOpacity>
             </View>
